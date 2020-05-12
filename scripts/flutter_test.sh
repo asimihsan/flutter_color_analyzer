a74449eeb82aa9c2737638fd41b013b40d1b3f46
Asim Ihsan <asim.ihsan@gmail.com>
2020-05-11 22:14:26 -0700

    Fix travis build

--- scripts/flutter_test.sh
+++ scripts/flutter_test.sh
@@ -4,8 +4,8 @@ set -eux
 
 cd $1
 flutter packages get
-flutter format --line-length 100 --set-exit-if-changed lib test
-flutter analyze
+flutter format --line-length 100 --set-exit-if-changed lib test example
+flutter analyze --no-current-package lib test example
 flutter test --no-pub --coverage
 # resets to the original state
 cd -
